7520ae4c880fc3ddc72a768afe2273e70e2e79c3
James Littlefield <jimilittle3@hotmail.com>
2020-02-17 21:29:33 -0500

    init commit, class comps, add vuetify, client server init setup structure

---
+++ client/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script lang="ts">
+import {Vue, Component} from 'vue-property-decorator'
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+@Component({
+  components: {
+    HelloWorld
+  }
+})
+export default class Home extends Vue {}
+</script>
