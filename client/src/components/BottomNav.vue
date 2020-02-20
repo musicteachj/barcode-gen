d9a436e4c83db79feb5254a8d64eb9570dcf6f8b
James Littlefield <jimilittle3@hotmail.com>
2020-02-19 21:57:49 -0500

    code cleanup

--- client/src/components/BottomNav.vue
+++ client/src/components/BottomNav.vue
@@ -1,4 +1,3 @@
-
 <template>
 
 <v-bottom-navigation
@@ -25,29 +24,6 @@
       <v-icon>mdi-printer</v-icon>
     </v-btn>
   </v-bottom-navigation>
-
-  <!-- <v-bottom-navigation
-    app
-    fixed
-    grow
-    v-model="activeItem"
-    color="primary"
-  >
-    <v-btn value="scan" :to="{ name: 'scan'}">
-      <span>Scan</span>
-      <v-icon>mdi-barcode-scan</v-icon>
-    </v-btn>
-
-    <v-btn value="create" :to="{ path: '/'}">
-      <span>Create</span>
-      <v-icon>mdi-barcode</v-icon>
-    </v-btn>
-
-    <v-btn value="print" :to="{ name: 'print'}">
-      <span>Print</span>
-      <v-icon>mdi-printer</v-icon>
-    </v-btn>
-  </v-bottom-navigation> -->
 
 </template>
 
--- client/src/components/TopToolbar.vue
+++ client/src/components/TopToolbar.vue
@@ -6,7 +6,6 @@
     tile
   >
     <v-toolbar dense>
-      <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
       <v-btn icon>
         <v-icon>{{routeIcons}}</v-icon>
       </v-btn>
@@ -15,61 +14,9 @@
 
       <v-spacer></v-spacer>
 
-      <!-- <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn> -->
       <v-toolbar-title>Barcode Gen</v-toolbar-title>
     </v-toolbar>
   </v-card>
-  <!-- <div>
-
-    <v-navigation-drawer
-      app
-      fixed
-      v-model="showMenu"
-
-    >
-      <v-list dense>
-        <v-list-item @click="doNothing">
-          <v-list-item-action>
-            <v-icon>settings</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Settings</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item @click="doNothing">
-          <v-list-item-action>
-            <v-icon>help</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Help</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-app-bar
-      app
-      fixed
-      dark
-      color="primary"
-    >
-      <v-app-bar-nav-icon @click.stop="toggleMenu"></v-app-bar-nav-icon>
-
-      <v-toolbar-title>Vue News</v-toolbar-title>
-
-    </v-app-bar>
-
-  </div> -->
 </template>
 
 <script lang="ts">
@@ -77,15 +24,6 @@ import { Component, Vue } from 'vue-property-decorator';
 
 @Component
 export default class TopToolbar extends Vue {
-
-  showMenu: boolean = false;
-
-  toggleMenu(): void {
-    this.showMenu = !this.showMenu;
-  }
-
-  doNothing(): void {
-  }
 
   get routeIcons() {
     if (this.$route.name === "Scan") {
