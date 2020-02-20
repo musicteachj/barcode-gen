5b6f0ba081ec652f3235d6af8e3e6dbbcb3bd569
James Littlefield <jimilittle3@hotmail.com>
2020-02-20 17:15:37 -0500

    adjust heading, barcode card styles, action btns

--- client/src/views/Print.vue
+++ client/src/views/Print.vue
@@ -1,24 +1,40 @@
 <template>
-
   <v-container>
-    <h1 class="headline">[[Print]]</h1>
-    <h2>Content is coming to this mobile app very soon!</h2>
+    <p class="display-4 text-center">Created Barcodes</p>
 
     <v-card
       outlined
-      @click="printBarcode"
       class="mx-auto cardClass"
       max-width="344"
       
     >
-    <h1>test</h1>
-    <VueBarcode  id="bar" value="123456789">
+    <h1 class="text-center">Category/Name</h1>
+    <h1 class="text-center">Type</h1>
+    <VueBarcode  class="text-center" id="bar" value="123456789">
       Show this if the rendering fails.
     </VueBarcode>
+
+    <v-card-actions>
+      <v-btn
+        color="error"
+        text
+      >
+        Delete
+      </v-btn>
+      
+      <v-spacer/>
+
+      <v-btn
+        color="primary"
+        text
+        @click="printBarcode"
+      >
+        Print
+      </v-btn>
+    </v-card-actions>
         
     </v-card>
   </v-container>
-
 </template>
 
 <script lang="ts">
