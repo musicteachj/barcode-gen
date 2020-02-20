4a79c1e7a115acba54c22028b49fa194fdb9fc07
James Littlefield <jimilittle3@hotmail.com>
2020-02-19 22:11:11 -0500

    add printd npm lib, add print func to barcode

--- client/src/views/Print.vue
+++ client/src/views/Print.vue
@@ -5,32 +5,16 @@
     <h2>Content is coming to this mobile app very soon!</h2>
 
     <v-card
+      @click="printBarcode"
       class="mx-auto"
       max-width="344"
       
     >
     <h1>test</h1>
-    <VueBarcode value="123456789">
+    <VueBarcode  id="bar" value="123456789">
       Show this if the rendering fails.
     </VueBarcode>
-          <!-- <v-list-item three-line>
-        <v-list-item-content>
-          <div class="overline mb-4">OVERLINE</div>
-          <v-list-item-title class="headline mb-1">Headline 5</v-list-item-title>
-          <v-list-item-subtitle>Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
-        </v-list-item-content>
-
-        <v-list-item-avatar
-          tile
-          size="80"
-          color="grey"
-        ></v-list-item-avatar>
-      </v-list-item>
-
-      <v-card-actions>
-        <v-btn text>Button</v-btn>
-        <v-btn text>Button</v-btn>
-      </v-card-actions> -->
+        
     </v-card>
   </v-container>
 
@@ -39,6 +23,15 @@
 <script lang="ts">
   import { Component, Vue } from 'vue-property-decorator';
   import VueBarcode from 'vue-barcode';
+  import { Printd } from 'printd'
+
+  const cssText = `
+  h1 {
+    color: black;
+    font-family: sans-serif;
+  }
+`
+
 
   @Component({
     components: {
@@ -46,5 +39,11 @@
     }
   })
   export default class Print extends Vue {
+
+    printBarcode() {
+      const d = new Printd()
+      d.print( document.getElementById('bar'), [ cssText ] )
+    }
+
   }
 </script>
