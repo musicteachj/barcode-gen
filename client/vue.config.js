b87ef0ca3cb3f70f85012b8ec3c29254e95e5601
James Littlefield <jimilittle3@hotmail.com>
2021-02-10 13:43:16 -0500

    edit vue config

--- client/vue.config.js
+++ client/vue.config.js
@@ -4,6 +4,7 @@ module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
+  productionSourceMap: false,
   outputDir: path.resolve(__dirname, '../server/public'),
   devServer: {
     proxy: {
