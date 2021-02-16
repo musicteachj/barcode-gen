464fb77178736ecbd9df41e38b84fefc56885235
James Littlefield <jimilittle3@hotmail.com>
2021-02-16 09:28:07 -0500

    remove public dir

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV === 'production') {
   // app.use(express.static(path.join(__dirname, 'public')));
 
   // Handle SPA
-  app.get('*', (req, res) => res.sendFile(__dirname + '/public/index.html'));
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
   // app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'public', 'index.html')));
 }
 
