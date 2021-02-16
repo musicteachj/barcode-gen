be57b1598a5a1715346ab0cd67260744fa6cf3e9
James Littlefield <jimilittle3@hotmail.com>
2021-02-16 09:18:51 -0500

    test

--- server/index.js
+++ server/index.js
@@ -17,11 +17,11 @@ app.use('/api/barcodes/', barcodes);
 if (process.env.NODE_ENV === 'production') {
   // Static folder
   // app.use(express.static(__dirname + '/public/'));
-  app.use(express.static(path.join(__dirname + '/public/')));
+  app.use(express.static(path.join(__dirname, 'public')));
 
   // Handle SPA
   // app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
-  app.get(/.*/, (req, res) => res.sendFile(path.resolve(__dirname + '/public/index.html')));
+  app.get(/.*/, (req, res) => res.sendFile(path.resolve(__dirname, 'public', 'index.html')));
 }
 
 const port = process.env.PORT || 3000;
